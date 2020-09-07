b4ef5ef646d63699a9fc37df75265f98e09c4297
agruenwald <a.gruenw@gmail.com>
2020-09-07 21:25:38 +0200

    Initial commit.

--- src/Command/EcsDeploymentCommand.php
+++ src/Command/EcsDeploymentCommand.php
@@ -1,10 +1,34 @@
 <?php
-
-
 namespace Pimcore\Cloud\AWS\Deployment\Command;
 
+use Symfony\Component\Console\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class EcsDeploymentCommand
+class EcsDeploymentCommand extends Command
 {
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'pimcore:cloud:aws:deployment';
 
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Command runs!');
+        // ... put here the code to run in your command
+
+        // this method must return an integer number with the "exit status code"
+        // of the command. You can also use these constants to make code more readable
+
+        // return this if there was no problem running the command
+        // (it's equivalent to returning int(0))
+        return Command::SUCCESS;
+
+        // or return this if some error happened during the execution
+        // (it's equivalent to returning int(1))
+        // return Command::FAILURE;
+    }
 }
